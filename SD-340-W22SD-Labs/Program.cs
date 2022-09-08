9da6376dd20fa7806f66bb61d7ef6b29c2ce78d0
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-09-08 10:12:40 -0500

    Initial example

--- SD-340-W22SD-Labs/Program.cs
+++ SD-340-W22SD-Labs/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿LinkedList<int> ints = new LinkedList<int>(1, 2);
+ints.PrintValues();
+
+class LinkedList<T>
+{
+    public T First { get; set; }
+    public T Second { get; set; }
+    public LinkedList(T first, T second)
+    {
+        First = first;
+        Second = second;
+    }
+
+    public void PrintValues()
+    {
+        Console.WriteLine($"Values: {First.ToString()} and {Second.ToString()}");
+    }
+}
