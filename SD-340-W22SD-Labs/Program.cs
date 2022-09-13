2237877e24b8ef857d4e98ba989ac730e453aac8
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-09-13 11:06:47 -0500

    Add another instance of a Linked List

--- SD-340-W22SD-Labs/Program.cs
+++ SD-340-W22SD-Labs/Program.cs
@@ -1,18 +1,28 @@
-﻿LinkedList<int> ints = new LinkedList<int>(1, 2);
+﻿LinkedList<int> ints = new LinkedList<int>(1, 2, 3);
+LinkedList<int> ints2 = new LinkedList<int>(20, 30, 40);
+
 ints.PrintValues();
+ints2.PrintValues();
 
 class LinkedList<T>
 {
     public T FirstNode { get; set; }
     public T SecondNode { get; set; }
-    public LinkedList(T first, T second)
+    public T ThirdNode { get; set; }
+    public LinkedList(T first, T second, T thirdNode)
     {
         FirstNode = first;
         SecondNode = second;
+        ThirdNode = thirdNode;
     }
 
+    public void Add(int value)
+    {
+        this.Add(value);
+    }
     public void PrintValues()
     {
-        Console.WriteLine($"Values: {FirstNode.ToString()} and {SecondNode.ToString()}");
+        Console.WriteLine($"Values: {FirstNode.ToString()}, {SecondNode.ToString()}, and {ThirdNode.ToString()}");
     }
 }
+
